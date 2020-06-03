45fc2d7b5f518c7722401aaded1683c39118e30e
rahul-mitra13 <rmitra@trincoll.edu>
2020-06-03 17:46:43 -0400

    yo3

--- collecter.py
+++ collecter.py
@@ -20,8 +20,8 @@ while True:
     else: #bringDown
         wiringpi.pwmWrite(18, 175)  #write PWM pulses of 175 to pin 18, rotating end onto rod
     GPIO.output(17,GPIO.LOW)
-    subprocess.run(["sudo","./adxl345spi","-t","1","-s",str(now)+"_"+str(flag)+"_"+"3.0_3200_5_.csv"])
+    subprocess.run(["sudo","./adxl345spi","-t","1","-s",str(now)+"_"+str(flag)+"_"+"3.0_3200_1.csv"])
     GPIO.output(17,GPIO.HIGH)
     flag = 1 - flag
-    time.sleep(180)
+    time.sleep(60)
     
