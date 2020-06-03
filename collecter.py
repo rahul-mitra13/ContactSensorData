115560f6d7e8c96f366e8056d78a7e427a70fe5f
rahul-mitra13 <rmitra@trincoll.edu>
2020-06-03 13:58:45 -0400

    all files

--- collecter.py
+++ collecter.py
@@ -1,7 +1,27 @@
+import os
+import time #library for sleep function
+import RPi.GPIO as GPIO #library for Raspberry Pi output pins
+import wiringpi #library for pwm control commands
 import subprocess
-import time
-t = 1707
+from time import localtime, strftime
+wiringpi.wiringPiSetupGpio()    #initiate GPIO pins
+wiringpi.pinMode(18, wiringpi.GPIO.PWM_OUTPUT)  #initialize pin 18 as a PWM output
+wiringpi.pwmSetMode(wiringpi.GPIO.PWM_MODE_MS)  #set pin 18 to 'milliseconds' mode
+# divide down clock
+wiringpi.pwmSetClock(192)   #set clock cycle speed of pwm output
+wiringpi.pwmSetRange(2000)  #range of potential pwm pulses to pass to the servo motor
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(17, GPIO.OUT)
+flag = 0
 while True:
-    subprocess.run(["sudo","./adxl345spi","-t","5","-s","0529"+str(t)+"_0_1.5_3200_5_.csv"])
+    now = strftime("%m%d%H%M", localtime())
+    if (flag == 0):#bringUp
+        wiringpi.pwmWrite(18, 150)  #write PWM pulses of 150 to pin 18, rotating end off of rod
+    else: #bringDown
+        wiringpi.pwmWrite(18, 175)  #write PWM pulses of 175 to pin 18, rotating end onto rod
+    GPIO.output(17,GPIO.LOW)
+    subprocess.run(["sudo","./adxl345spi","-t","5","-s",str(now)+"_"+str(flag)+"_"+"1.5_3200_5_.csv"])
+    GPIO.output(17,GPIO.HIGH)
+    flag = 1 - flag
     time.sleep(300)
-    t = t + 5 
+    
